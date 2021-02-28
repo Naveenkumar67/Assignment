034a5c11cfc6943fb9f4dee7d856962f3c7716ef
Naveenkumar67 <naveenvempati4@gmail.com>
2021-03-01 02:23:54 +0530

    second

--- index.js
+++ index.js
@@ -15,18 +15,64 @@ function val(){
         z.style.visibility="hidden";
     }
 }
-console.log(x);
 
-let dob=document.getElementById("dob").value;
+
+function da(){
+    let dob=document.getElementById("dob").value;
 let d= new Date();
 let y=d.getFullYear();
+let m=d.getMonth();
+let da=d.getDay();
 let s="";
+let x="";
+let b="";
+for(let i=5;i<7;i++){
+    x+=dob[i];
+}
+for(let i=8;i<10;i++){
+    b+=dob[i];
+}
 for(let i=0;i<4;i++){
     s+=dob[i];
 }
-if(y-parseInt(s)<18){
-    let x=document.getElementById("par1");
-    par1.innerHTML="Should be above 18 years";
+if(y-parseInt(s)>18 ){
+    let h=document.getElementById("par1");
+    h.style.visibility="hidden";
+}
+else if(y===parseInt(s)){
+    if(m>parseInt(x)){
+        let h=document.getElementById("par1");
+    h.style.visibility="hidden";
+    }
+    else if(m<parseInt(x)){
+        if(da>=parseInt(b)){
+            let h=document.getElementById("par1");
+        h.style.visibility="hidden";
+        }
+        else{
+            let h=document.getElementById("par1");
+            h.innerHTML="You must be 18 years old";
+        }
+    }
+    else{
+        let h=document.getElementById("par1");
+        h.innerHTML="You must be 18 years old";
+    }
+}
+// else if(y==parseInt(s) && m==parseInt(x)){
+//     if(da>=parseInt(b)){
+//         let h=document.getElementById("par1");
+//     h.style.visibility="hidden";
+//     }
+//     else{
+//         let h=document.getElementById("par1");
+//         h.innerHTML="You must be 18 years old";
+//     }
+// }
+else{
+    let h=document.getElementById("par1");
+    h.innerHTML="You must be 18 years old";
+}
 }
 
 
@@ -44,21 +90,21 @@ function pasval(){
     }
 }
 
-var a=document.getElementById("country")
-if(a.value=='India'){
-    let s1=document.getElementById("s1");
-    s1.innerHTML="Telangana";
-    let s2=document.getElementById("s2");
-    s2.innerHTML="Maharashtra";
-    let s3=document.getElementById("s3");
-    s3.innerHTML="Kerala";
+// var a=document.getElementById("country")
+// if(a.value=='India'){
+//     let s1=document.getElementById("s1");
+//     s1.innerHTML="Telangana";
+//     let s2=document.getElementById("s2");
+//     s2.innerHTML="Maharashtra";
+//     let s3=document.getElementById("s3");
+//     s3.innerHTML="Kerala";
 
-}
-else if(a.value=='America'){
-    let s1=document.getElementById("s1");
-    s1.innerHTML="California";
-    let s2=document.getElementById("s2");
-    s2.innerHTML="Texas";
-    let s3=document.getElementById("s3");
-    s3.innerHTML="Telangana";
-}
+// }
+// else if(a.value=='America'){
+//     let s1=document.getElementById("s1");
+//     s1.innerHTML="California";
+//     let s2=document.getElementById("s2");
+//     s2.innerHTML="Texas";
+//     let s3=document.getElementById("s3");
+//     s3.innerHTML="Telangana";
+// }
